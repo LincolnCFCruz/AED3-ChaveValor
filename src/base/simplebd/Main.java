6d95d53acc834e0b24d962196371aa78395a8053
LincolnCFCruz <lincolncfcruz@outlook.com>
2021-11-01 11:56:58 -0300

    use of methods and new function "Add"
    -reworked part of the code for method usage and better readability .
    -new method that adds lines inside the file.

--- src/base/simplebd/Main.java
+++ src/base/simplebd/Main.java
@@ -4,18 +4,129 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        BufferedReader br = null;
+    public static void create () {
+        try {
+            File file = new File("simpledb.db");
 
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            Scanner sc = new Scanner(System.in);
+            PrintWriter pw = new PrintWriter(file);
+            int loop;
+            String conteudo;
+
+            System.out.println("Numero de linhas.");
+            loop = sc.nextInt();
+            for (int i = 0; i<loop; i++) {
+                System.out.println("linha: ");
+                conteudo = sc.next();
+                pw.print(conteudo);
+                pw.print("\n");
+            }
+            pw.close();
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void add () {
+        File fArquivo = null;
+        try {
+            fArquivo = new File("simpledb.db");
+            FileWriter fwArquivo = null;
+
+            if (fArquivo.exists() == true) {
+                fwArquivo = new FileWriter(fArquivo, true);
+            } else {
+                fwArquivo = new FileWriter(fArquivo);
+            }
+
+            Scanner sc2 = new Scanner(System.in);
+            BufferedWriter bw = new BufferedWriter(fwArquivo);
+            String registro = sc2.nextLine();
+
+            bw.write(registro + "\n");
+
+            System.out.println("Done");
+
+            bw.close();
+            fwArquivo.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void read () {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("simpledb.db"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void update (){
+        Scanner sc1 = new Scanner(System.in);
+        File fileToBeModified = new File("simpledb.db");
+        String oldContent = "";
+        BufferedReader reader = null;
+        FileWriter writer = null;
+
+        System.out.println("Linha a ser alterada:");
+        String oldString = sc1.next();
+        System.out.println("Nova linha: ");
+        String newString = sc1.next();
+
+        try{
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+            String line = reader.readLine();
+            while (line != null) {
+                oldContent = oldContent + line + System.lineSeparator();
+                line = reader.readLine();
+            }
+            String newContent = oldContent.replaceAll(oldString,newString);
+            writer = new FileWriter((fileToBeModified));
+            writer.write(newContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                reader.close();
+                writer.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         int option;
         boolean flag = true;
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Opcao 1: Create");
-        System.out.println("Opcao 2: Read");
-        System.out.println("Opcao 3: Update");
-        System.out.println("Opcao 4: Delete");
-        System.out.println("Opcao 5: Exit");
+        System.out.println("Opcao 2: Add");
+        System.out.println("Opcao 3: Read");
+        System.out.println("Opcao 4: Update");
+        System.out.println("Opcao 5: Delete");
+        System.out.println("Opcao 0: Exit");
 
         while (flag == true) {
             System.out.println("Digite sua opcao:");
@@ -23,89 +134,21 @@ public class Main {
 
             switch (option) {
                 case 1: //Create
-                    try {
-                        File file = new File("simpledb.db");
-
-                        if (!file.exists()) {
-                            file.createNewFile();
-                        }
-
-                        PrintWriter pw = new PrintWriter(file);
-                        int loop;
-                        String conteudo;
-
-                        System.out.println("Numero de linhas.");
-                        loop = entrada.nextInt();
-                        for (int i = 0; i<loop; i++) {
-                            System.out.println("linha: ");
-                            conteudo = entrada.next();
-                            pw.print(conteudo);
-                            pw.print("\n");
-                        }
-                        pw.close();
-                        System.out.println("Done");
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    create();
                     break;
-                case 2: //Read
-                    try {
-
-                        br = new BufferedReader(new FileReader("simpledb.db"));
-                        String line;
-
-                        while ((line = br.readLine()) != null) {
-                            System.out.println(line);
-                        }
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            br.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                case 2:
+                    add();
                     break;
-                case 3: //Update
-                    File fileToBeModified = new File("simpledb.db");
-                    String oldContent = "";
-                    BufferedReader reader = null;
-                    FileWriter writer = null;
-                    System.out.println("Linha a ser alterada:");
-                    String oldString;
-                    oldString = entrada.next();
-                    System.out.println("Nova linha: ");
-                    String newString;
-                    newString = entrada.next();
-
-                    try{
-                        reader = new BufferedReader(new FileReader(fileToBeModified));
-                        String line = reader.readLine();
-                        while (line != null) {
-                            oldContent = oldContent + line + System.lineSeparator();
-                            line = reader.readLine();
-                        }
-                        String newContent = oldContent.replaceAll(oldString,newString);
-                        writer = new FileWriter((fileToBeModified));
-                        writer.write(newContent);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try{
-                            reader.close();
-                            writer.close();
-                        } catch (IOException e){
-                            e.printStackTrace();
-                        }
-                    }
+                case 3: //Read
+                    read();
                     break;
-                case 4:
+                case 4: //Update
+                    update();
+                    break;
+                case 5: //Delete
                     System.out.println("Delete");
                     break;
-                case 5:
+                case 0:
                     flag = false;
                     break;
                 default:
