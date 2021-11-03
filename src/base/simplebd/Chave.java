3b0ddac6d577210cdc8986c0ad87a2a6fd5e1728
LincolnCFCruz <lincolncfcruz@outlook.com>
2021-11-02 22:34:37 -0300

    Preparation for DataBase
    -added index
    -added type

--- src/base/simplebd/Chave.java
+++ src/base/simplebd/Chave.java
@@ -4,20 +4,23 @@ import java.io.*;
 import java.text.DecimalFormat;
 
 public class Chave {
-    protected int idLivro;
+    protected int index;
+    protected int type;
     protected String titulo;
     protected String autor;
     protected float preco;
 
-    public Chave(int i, String t, String a, float p) {
-        this.idLivro = i;
+    public Chave(int i,int q, String t, String a, float p) {
+        this.index = i;
+        this.type = q;
         this.titulo = t;
         this.autor = a;
         this.preco = p;
     }
 
     public Chave() {
-        this.idLivro = -1;
+        this.index = -1;
+        this.type = -1;
         this.titulo = "";
         this.autor = "";
         this.preco = 0F;
@@ -26,14 +29,15 @@ public class Chave {
     public String toString() {
         DecimalFormat df = new DecimalFormat("#,##0.00");
 
-        return "\nID....: " + this.idLivro + "\nTítulo: " + this.titulo + "\nAutor.: " + this.autor + "\nPreço.: R$ "
+        return "\nID....: " + this.index + "\nType...: " + this.type + "\nTítulo: " + this.titulo + "\nAutor.: " + this.autor + "\nPreço.: R$ "
                 + df.format(this.preco);
     }
 
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
-        dos.writeInt(idLivro);
+        dos.writeInt(index);
+        dos.writeInt(type);
         dos.writeUTF(titulo);
         dos.writeUTF(autor);
         dos.writeFloat(preco);
@@ -43,7 +47,8 @@ public class Chave {
     public void fromByteArray(byte[] ba) throws IOException {
         ByteArrayInputStream bais = new ByteArrayInputStream(ba);
         DataInputStream dis = new DataInputStream(bais);
-        idLivro = dis.readInt();
+        index = dis.readInt();
+        type = dis.readInt();
         titulo = dis.readUTF();
         autor = dis.readUTF();
         preco = dis.readFloat();
